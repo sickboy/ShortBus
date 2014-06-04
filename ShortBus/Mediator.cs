e6ca8ec9c4bc7dd3d63b7e0b06832bcaccb67eaa
Patrick Roza <roza@sixnetworks.net>
2014-06-04 11:13:25 +0200

    Handle null arguments

--- ShortBus/Mediator.cs
+++ ShortBus/Mediator.cs
@@ -20,23 +20,35 @@ namespace ShortBus
         readonly IDependencyResolver _dependencyResolver;
 
         public Mediator(IDependencyResolver dependencyResolver) {
+            if (dependencyResolver == null)
+                throw new ArgumentNullException("dependencyResolver");
+
             _dependencyResolver = dependencyResolver;
             HandlerInstanceBuilder = dependencyResolver.GetInstance;
         }
 
         public virtual TResponseData Request<TResponseData>(IRequest<TResponseData> request) {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
             return
                 HandlerInstanceBuilder(typeof (IRequestHandler<,>).MakeGenericType(request.GetType(),
                     typeof (TResponseData))).Handle((dynamic) request);
         }
 
         public Task<TResponseData> RequestAsync<TResponseData>(IAsyncRequest<TResponseData> request) {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
             return
                 HandlerInstanceBuilder(typeof (IAsyncRequestHandler<,>).MakeGenericType(request.GetType(),
                     typeof (TResponseData))).HandleAsync((dynamic) request);
         }
 
         public void Notify<TNotification>(TNotification notification) {
+            if (notification == null)
+                throw new ArgumentNullException("notification");
+
             var handlers = _dependencyResolver.GetInstances<INotificationHandler<TNotification>>();
 
             List<Exception> exceptions = null;
@@ -53,6 +65,9 @@ namespace ShortBus
         }
 
         public Task NotifyAsync<TNotification>(TNotification notification) {
+            if (notification == null)
+                throw new ArgumentNullException("notification");
+
             var handlers = _dependencyResolver.GetInstances<IAsyncNotificationHandler<TNotification>>();
 
             return Task.WhenAll(handlers.Select(x => x.HandleAsync(notification)));
