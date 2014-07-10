7020ddf59f088dc0ef79dd6b54b5fe5d44b8c068
Patrick Roza <roza@sixnetworks.net>
2014-07-10 19:50:29 +0200

    Added Mef2 and SimpleInjector resolvers.

---
+++ ShortBus.Mef2/Mef2DependencyResolver.cs
@@ -0,0 +1,27 @@
+// <copyright company="SIX Networks GmbH">
+//     Copyright (c) SIX Networks GmbH. All rights reserved. Do not remove this notice.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+
+namespace ShortBus.Mef
+{
+    public class MefDependencyResolver : IDependencyResolver
+    {
+        readonly CompositionContext _container;
+
+        public MefDependencyResolver(CompositionContext container) {
+            _container = container;
+        }
+
+        public object GetInstance(Type type) {
+            return _container.GetExport(type);
+        }
+
+        public IEnumerable<T> GetInstances<T>() {
+            return _container.GetExports<T>();
+        }
+    }
+}
---
+++ ShortBus.SimpleInjector/SimpleInjectorDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿// <copyright company="SIX Networks GmbH">
+//     Copyright (c) SIX Networks GmbH. All rights reserved. Do not remove this notice.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using SimpleInjector;
+
+namespace ShortBus.SimpleInjector
+{
+    public class SimpleInjectorDependencyResolver : IDependencyResolver
+    {
+        readonly Container _container;
+
+        public SimpleInjectorDependencyResolver(Container container) {
+            _container = container;
+        }
+
+        public object GetInstance(Type type) {
+            return _container.GetInstance(type);
+        }
+
+        public IEnumerable<T> GetInstances<T>() {
+            return _container.GetAllInstances<T>();
+        }
+    }
+}
